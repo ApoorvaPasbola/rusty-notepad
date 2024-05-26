73d97046171d3db78f91949c329906b2c0ead4fa
Apoorva Pasbola <pasbola.apoorva@gmail.com>
2024-05-26 16:43:59 +0530

    restructuring ui code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from '@tauri-apps/api';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   greetingMessage = "";
