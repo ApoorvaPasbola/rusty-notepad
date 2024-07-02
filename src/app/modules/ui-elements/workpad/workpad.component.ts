2ab68fda1839ea5f840d010fa2fd7da35c3bc300
Apoorva Pasbola <pasbola.apoorva@gmail.com>
2024-07-02 21:37:46 +0530

    WIP - numbered rows

--- src/app/modules/ui-elements/workpad/workpad.component.ts
+++ src/app/modules/ui-elements/workpad/workpad.component.ts
@@ -1,19 +1,34 @@
 import {
   Component,
+  ElementRef,
+  HostListener,
+  Signal,
+  ViewChild,
+  signal,
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ResizerDirective } from '../../directives/resizer/resizer.directive';
 
 @Component({
   selector: 'app-workpad',
   standalone: true,
-  imports: [CommonModule, FormsModule, ResizerDirective],
+  imports: [CommonModule, FormsModule, ResizerDirective, NgFor],
   templateUrl: './workpad.component.html',
   styleUrl: './workpad.component.scss',
 })
 
 export class WorkpadComponent {
+
+  rows:number = 1
+  
+  @ViewChild("textarea") textarea!: ElementRef<HTMLTextAreaElement>
+  @HostListener("input") onInput() {
+      this.rows = this.textarea.nativeElement.value.split("\n").length;
+  }
+
+  
+  
   work: string = '';
 
 }
