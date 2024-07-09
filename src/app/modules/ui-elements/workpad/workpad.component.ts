a2175584c466dd21db98bca322547bdae9b0c684
Apoorva Pasbola <pasbola.apoorva@gmail.com>
2024-07-09 13:07:39 +0530

    WIP: vanilla workpad from scratch

--- src/app/modules/ui-elements/workpad/workpad.component.ts
+++ src/app/modules/ui-elements/workpad/workpad.component.ts
@@ -1,5 +1,4 @@
 import {
-  AfterViewInit,
   Component,
   ElementRef,
   OnInit,
@@ -8,7 +7,6 @@ import {
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ResizerDirective } from '../../directives/resizer/resizer.directive';
-import Quill from 'quill';
 
 @Component({
   selector: 'app-workpad',
@@ -23,32 +21,22 @@ export class WorkpadComponent implements OnInit {
   @ViewChild("editorContainer", { static: true })
   editorContainer: ElementRef | null = null;
 
-  quill: Quill | undefined
 
 
   ngOnInit(): void {
-    if (this.editorContainer) {
-      this.quill = new Quill(this.editorContainer.nativeElement, {
-        modules: {
-          toolbar: [
-            [{ header: [1, 2, false] }],
-            ["bold", "italic", "underline", "strike"],
-            [{ list: "ordered" }, { list: "bullet" }],
-            ["link", "image", "video"],
-            [{ align: [] }],
-            [{ color: [] }, { background: [] }],
-            ["clean"],
-          ],
-        },
-        theme: "snow"
-      });
-    }
+
   }
 
   getEditorContent() {
-    if (!this.quill)
-      return "";
-    return this.quill?.root.innerHTML;
+
   }
+  formatText(command:any, value = undefined) {
+    document.execCommand(command, false, value);
+  }
+  createLink(){
+
+  }
+
+
 
 }
